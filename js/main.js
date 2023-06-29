8a814a6e3d7f7207d17fd6794f791d4374487eaa
Sandy camargo <sandyvanessa95@outlook.com>
2023-06-29 15:51:52 +0200

    ex

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*ESERCIZIO 
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla console nomecognomecolorepreferito21*/
+
+let name = prompt("inserisci il tuo nome");
+let surname = prompt("inserisci cognome");
+let Colors = prompt("dimmi il tuo colore preferito");
+
+
